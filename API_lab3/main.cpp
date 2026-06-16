dd3a00ab03ff35e03c926a615524f9f09c11863e
Danko <bogdbutk-ipt28@lll.kpi.ua>
2026-06-16 12:33:57 +0300

    bit format

--- API_lab3/main.cpp
+++ API_lab3/main.cpp
@@ -1,10 +1,15 @@
 #include "header.h"
 
+void printBits(const uint8_t* buffer, uint32_t n) {
+    for (uint32_t i = 0; i < n; i++)
+        cout << ((buffer[i / 8] >> (i % 8)) & 1);
+}
+
 int main() {
-    uint8_t a1[] = { 0xE1, 0x01 };  // '100001111'
-    uint8_t a2[] = { 0xEE, 0x00 };  // '011101110'
-    uint8_t a3[] = { 0xFF };        // '11111111'
-    uint8_t a4[] = { 0xAA };        // '01010101'
+    uint8_t a1[] = { 0xE1, 0x01 };
+    uint8_t a2[] = { 0xEE, 0x00 };
+    uint8_t a3[] = { 0xFF };
+    uint8_t a4[] = { 0xAA };
 
     BitWriter w("test.bin");
     w.writeBits(a1, 9);
@@ -12,6 +17,33 @@ int main() {
     w.writeBits(a3, 8);
     w.writeBits(a4, 4);
     w.close();
+
+    cout << "Written:\n";
+    cout << "a1  (9 bits): "; 
+    printBits(a1, 9); 
+    cout << "\n";
+    
+    cout << "a2  (9 bits): "; 
+    printBits(a2, 9); 
+    cout << "\n";
+    
+    cout << "a3  (8 bits): "; 
+    printBits(a3, 8); 
+    cout << "\n";
+    
+    cout << "a4  (4 bits): "; 
+    printBits(a4, 4); 
+    cout << "\n\n";
+
+    cout << "Full stream:\n";
+    ifstream f("test.bin", ios::binary);
+    uint8_t byte;
+    while (f.read(reinterpret_cast<char*>(&byte), 1)) {
+        for (int i = 0; i < 8; i++)
+            cout << ((byte >> i) & 1);
+        cout << " ";
+    }
+    cout << "\n\n";
 
     uint8_t b1[2], b2[1], b3[2], b4[1];
 
@@ -22,10 +54,21 @@ int main() {
     r.readBits(b4, 4);
     r.close();
 
-    printf("b1 (11 bits): %02X %02X\n", b1[0], b1[1]);
-    printf("b2  (7 bits): %02X\n", b2[0]);
-    printf("b3  (8 bits): %02X\n", b3[0]);
-    printf("b4  (4 bits): %02X\n", b4[0]);
+    cout << "Read:\n";
+    cout << "b1 (11 bits): "; 
+    printBits(b1, 11); cout << "\n";
+    
+    cout << "b2  (7 bits): "; 
+    printBits(b2, 7); 
+    cout << "\n";
+    
+    cout << "b3  (8 bits): "; 
+    printBits(b3, 8); 
+    cout << "\n";
+    
+    cout << "b4  (4 bits): "; 
+    printBits(b4, 4); 
+    cout << "\n";
 
     return 0;
 }
