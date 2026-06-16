9b532f9102f395070646cf2ca330aca7f66c54b6
Danko <bogdbutk-ipt28@lll.kpi.ua>
2026-06-16 22:46:14 +0300

    main final

--- API_lab3/main.cpp
+++ API_lab3/main.cpp
@@ -18,11 +18,11 @@ void analyze(const string& input) {
 
 int main() {
     cout << "\n--- JSON ---\n";
-    analyze("File_lab4/jcon/fingerprint.json");
-    analyze("File_lab4/jcon/simd_f32x4_cmp.json");
-    analyze("File_lab4/jcon/simd_f32x4_pmin_pmax.json");
-    analyze("File_lab4/jcon/simd_f64x2_cmp.json");
-    analyze("File_lab4/jcon/simd_f64x2_pmin_pmax.json");
+    analyze("File_lab4/json/fingerprint.json");
+    analyze("File_lab4/json/simd_f32x4_cmp.json");
+    analyze("File_lab4/json/simd_f32x4_pmin_pmax.json");
+    analyze("File_lab4/json/simd_f64x2_cmp.json");
+    analyze("File_lab4/json/simd_f64x2_pmin_pmax.json");
 
     cout << "\n--- EXE ---\n";
     analyze("File_lab4/exe/AJAZZ_AK820.exe");
@@ -38,19 +38,19 @@ int main() {
     analyze("File_lab4/txt/results.txt");
     analyze("File_lab4/txt/text.txt");
 
-    cout << "\n--- PNG ---\n";
-    analyze("File_lab4/png/7.jpg");
-    analyze("File_lab4/png/8.jpg");
-    analyze("File_lab4/png/9.jpg");
-    analyze("File_lab4/png/10.jpg");
-    analyze("File_lab4/png/11.jpg");
+    cout << "\n--- JPG ---\n";
+    analyze("File_lab4/jpg/7.jpg");
+    analyze("File_lab4/jpg/8.jpg");
+    analyze("File_lab4/jpg/9.jpg");
+    analyze("File_lab4/jpg/10.jpg");
+    analyze("File_lab4/jpg/11.jpg");
 
     cout << "\n--- MP3 ---\n";
-    analyze("File_lab4/mp3/Ruth_B.mp3");
-    analyze("File_lab4/mp3/27964292_uplifting.mp3");
-    analyze("File_lab4/mp3/tik-alkogolzm.mp3");
-    analyze("File_lab4/mp3/Evan_Call.mp3");
-    analyze("File_lab4/mp3/Lukrembo_Onion.mp3");
+    analyze("File_lab4/mp3/music1.mp3");
+    analyze("File_lab4/mp3/music2.mp3");
+    analyze("File_lab4/mp3/music3.mp3");
+    analyze("File_lab4/mp3/music4.mp3");
+    analyze("File_lab4/mp3/music5.mp3");
 
     return 0;
 }
